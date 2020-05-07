a8d4379963ef6a392810545519425d3f0fd01dea
ferilnx <64975219+ferilnx@users.noreply.github.com>
2020-05-07 22:43:22 +0300

    esx_pizza
    Türkçeleştirilmiş esx_pizza scripti.

---
+++ esx_pizza/locales/tr.lua
@@ -0,0 +1,36 @@
+                                                                 
+                                                                      --https://www.fivemturk.com/
+																 --Feril Tarafından Türkçeleştirilmiştir--
+
+Locales['tr'] = {
+	['cloakroom']				= 'Kıyafet Dolabı',
+	['job_wear']				= 'İş Kıyafeti',
+	['citizen_wear']			= 'Sivil Kıyafeti',
+	['vehiclespawner']			= 'Aracı Seçimi', 
+	['already_have_truck']		= 'Zaten araca sahipsin', 
+	['delivery']				= '~INPUT_PICKUP~ ve teslim et.',
+	['not_your_truck']			= 'İş aracında olmanız gerekiyor!',
+	['not_your_truck2']			= 'İş aracınızda olmanız gerekiyor !',
+	['need_it']					= 'Evet,ihtiyacımız var!',
+	['ok_work']					= 'Tamamdır!',
+	['scared_me']				= 'Tamam beni korkuttun !',
+	['resume_delivery']			= 'Tamam teslimatınızı devam ettirin!',
+	['no_delivery']				= 'Teslimat yoksa Nakit Yok',
+	['pay_repair']				= 'Aracı çarpar iseniz tamir ücretini maaşınızdan keseceğiz !',
+	['repair_minus']			= 'Araç Onarımı : -',
+	['shipments_plus']			= 'Ücret : +',
+	['truck_state']				= 'Para yok dostum!',
+	['no_delivery_no_truck']	= 'Teslimat yok,kamyon yok bu bir şakamı yoksa?',
+	['truck_price']				= 'Araç Ücreti : -',
+	['meet_ls']					= 'Sarı ile işaretlenmiş teslimat noktasına git!',
+	['meet_bc']					= 'Sarı ile işaretlenmiş teslimat noktasına git!',
+	['meet_del']				= 'Teslimat Noktasına Git',
+	['return_depot']			= 'Depoya Geri Dön',
+	['blip_job']				= 'Dominos Pizza',
+	['blip_delivery']			= 'Dominos Pizza : Delivery',
+	['blip_goal']				= 'Dominos Pizza : Delivery point',
+	['blip_depot']				= 'Dominos Pizza : depot',
+}
+
+                                                                       --https://www.fivemturk.com/ 
+                                                                  --Feril Tarafından Türkçeleştirilmiştir--
